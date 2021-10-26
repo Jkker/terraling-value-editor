b5d4fd5c400a8bab6239e46895c8696e48330251
jkker <me@jerrykjia.com>
2021-10-26 03:28:32 -0400

    feat: routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import DetailedEditor from './pages/DetailedEditor';
+import DetailedEditor from './pages/ListEditor';
 import TableEditor from './pages/TableEditor';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Test from './Test';
 
 const theme = createTheme({
 	palette: {
@@ -22,8 +24,19 @@ const theme = createTheme({
 ReactDOM.render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
-			<DetailedEditor />
-			{/* <TableEditor /> */}
+			<Router>
+				<Switch>
+					<Route path='/table'>
+						<TableEditor />
+					</Route>
+					<Route path='/test'>
+						<Test />
+					</Route>
+					<Route path='/'>
+						<DetailedEditor />
+					</Route>
+				</Switch>
+			</Router>
 		</ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
